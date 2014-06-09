d0d9d69c40854ed94bb0b656c67f0d421398e1d4
Guinsly Mondésir <guinslym@gmail.com>
2014-06-09 01:46:12 -0400

    adding root in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 PaperclipTuts::Application.routes.draw do
   resources :listings
+  root 'listings#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
